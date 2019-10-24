0458fd609803541d606c79408fb8ddb2391db73c
Martin Guether <mg@magegu.com>
2019-10-24 17:50:52 +0200

    update

--- verifyCredentials.js
+++ verifyCredentials.js
@@ -1,34 +1,33 @@
 "use strict";
-const request = require('request-promise');
+const request = require("request-promise");
 
 module.exports = verify;
+const base = "https://api.dropscan.de/v1";
 
-/**
- * Executes the verification logic by sending a simple to the Petstore API using the provided apiKey.
- * If the request succeeds, we can assume that the apiKey is valid. Otherwise it is not valid.
- *
- * @param credentials object to retrieve apiKey from
- *
- * @returns Promise sending HTTP request and resolving its response
- */
-function verify(credentials) {
+async function verify(credentials) {
+  try {
+    const accessToken = credentials.oauth.oauth_token;
 
-    // access the value of the apiKey field defined in credentials section of component.json
-    const apiKey = credentials.apiKey;
-
-    if (!apiKey) {
-        throw new Error('API key is missing');
-    }
-
-    // sending a request to the most simple endpoint of the target API
     const requestOptions = {
-        uri: 'https://petstore.elastic.io/v2/user/me',
-        headers: {
-            'api-key': apiKey
-        },
-        json: true
+      uri: `${base}/scanboxes`,
+      headers: {
+        Authorization: `Bearer ${accessToken}`
+      },
+      json: true
     };
 
-    // if the request succeeds, we can assume the api key is valid
-    return request.get(requestOptions);
+    const valid = await request.get(requestOptions);
+
+    console.log(JSON.stringify(valid));
+    if (valid) {
+      console.log("Credentials verified successfully");
+      return true;
+    } else {
+      throw new Error("Error in validating credentials!");
+      return false;
+    }
+  } catch (e) {
+    console.log(`${e}`);
+    throw new Error(e);
+  }
 }
